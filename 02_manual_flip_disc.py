89563cbabc474fdc958bf8e727cfcd3e29213035
Irakli Mirotadze <imiro22@freeuni.edu.ge>
2026-03-28 12:56:47 +0400

    refactor: manual flip disc

--- 02_manual_flip_disc.py
+++ 02_manual_flip_disc.py
@@ -6,6 +6,15 @@ ensure_local_python()
 
 from rigid_body_core import BaseSimulationApp, mat_identity, mat_mul, orthonormalize, rotation_matrix
 
+ROTATION_CONTROLS = [
+    ("a", (1.0, 0.0, 0.0), 1.0, 0),
+    ("d", (1.0, 0.0, 0.0), -1.0, 0),
+    ("w", (0.0, 1.0, 0.0), 1.0, 1),
+    ("s", (0.0, 1.0, 0.0), -1.0, 1),
+    ("q", (0.0, 0.0, 1.0), 1.0, 2),
+    ("e", (0.0, 0.0, 1.0), -1.0, 2),
+]
+
 
 class ManualFlipSimulation(BaseSimulationApp):
     def __init__(self) -> None:
@@ -18,24 +27,11 @@ class ManualFlipSimulation(BaseSimulationApp):
 
     def update(self, dt: float) -> None:
         angle = self.step_speed * dt
-        if "a" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((1.0, 0.0, 0.0), angle)))
-            self.highlight_axis = 0
-        elif "d" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((1.0, 0.0, 0.0), -angle)))
-            self.highlight_axis = 0
-        elif "w" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((0.0, 1.0, 0.0), angle)))
-            self.highlight_axis = 1
-        elif "s" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((0.0, 1.0, 0.0), -angle)))
-            self.highlight_axis = 1
-        elif "q" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((0.0, 0.0, 1.0), angle)))
-            self.highlight_axis = 2
-        elif "e" in self.pressed_keys:
-            self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix((0.0, 0.0, 1.0), -angle)))
-            self.highlight_axis = 2
+        for key, axis, direction, highlight in ROTATION_CONTROLS:
+            if key in self.pressed_keys:
+                self.orientation = orthonormalize(mat_mul(self.orientation, rotation_matrix(axis, direction * angle)))
+                self.highlight_axis = highlight
+                break
 
     def status_lines(self) -> list[str]:
         return [
